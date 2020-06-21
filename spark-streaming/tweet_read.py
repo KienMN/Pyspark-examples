fa9f7c5cb07df1dbb05893fc30fb7249a3bf6371
KienMN <kienmn97@gmail.com>
2020-06-21 22:21:39 +0900

    PySpark streaming Twitter tweets

---
+++ spark-streaming/tweet_read.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import tweepy
+from tweepy import OAuthHandler, Stream
+from tweepy.streaming import StreamListener
+import socket
+import json
+
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
+
+class TweetListener(StreamListener):
+    def __init__(self, csocket):
+        self.client_socket = csocket
+        
+    def on_data(self, data):
+        try:
+            msg = json.loads(data)
+            print(msg['text'].encode('utf-8'))
+            self.client_socket.send(msg['text'].encode('utf-8'))
+            return True
+        except BaseException as e:
+            print("ERROR", e)
+        return True
+    
+    def on_error(self, status):
+        print(status)
+        return True
+
+def send_data(c_socket):
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+    
+    twitter_stream = Stream(auth, TweetListener(c_socket))
+    twitter_stream.filter(track=['soccer'])
+
+if __name__ == '__main__':
+    s = socket.socket()
+    host = '127.0.0.1'
+    port = 9998
+    s.bind((host, port))
+    
+    print('Listening on port {}'.format(port))
+    
+    s.listen(10)
+    c, addr = s.accept()
+    send_data(c)
